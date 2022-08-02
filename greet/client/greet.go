578ac699913e08946be36c0f8f9c6662443c7c65
Nikhil Ikhar <nikhil.ikhar@gmail.com>
2022-08-02 11:16:40 +0530

    Added gRPC server Streaming

--- greet/client/greet.go
+++ greet/client/greet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 
 	pb "github.com/nik-hil/go-grpc-course/greet/proto"
@@ -17,5 +18,28 @@ func doGreet(c pb.GreetServiceClient) {
 		log.Fatalf("Could not greet: %v\n", err)
 	}
 	log.Printf("Greeting: %s\n", res.Result)
+
+}
+
+func doGreetManyTimes(c pb.GreetServiceClient) {
+	log.Printf("doGreetManyTimes was invoked")
+
+	req := &pb.GreetRequest{
+		FirstName: "Nikhil",
+	}
+	stream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling doGreetManyTimes")
+	}
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while reading the stream: %v\n", err)
+		}
+		log.Printf("doGreetManyTimes %s\n", msg.Result)
+	}
 
 }
--- greet/server/greet.go
+++ greet/server/greet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	pb "github.com/nik-hil/go-grpc-course/greet/proto"
@@ -12,4 +13,16 @@ func (s *Server) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetRespo
 	return &pb.GreetResponse{
 		Result: "Hello " + in.FirstName,
 	}, nil
+}
+
+func (s *Server) GreetManyTimes(in *pb.GreetRequest, stream pb.GreetService_GreetManyTimesServer) error {
+	log.Printf("GreetManyTimes function was invoked with %v", in)
+
+	for i := 0; i < 10; i++ {
+		res := fmt.Sprintf("Hello %s count %d", in.FirstName, i)
+		stream.Send(&pb.GreetResponse{
+			Result: res,
+		})
+	}
+	return nil
 }
